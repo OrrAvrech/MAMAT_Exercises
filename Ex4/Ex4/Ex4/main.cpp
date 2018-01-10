02e808345b91af8b1ec9db2080f0dc4501be165f
DeanBarzilay <34457749+DeanBarzilay@users.noreply.github.com>
2018-01-10 20:27:07 +0200

    finished main and office

--- Ex4/Ex4/Ex4/main.cpp
+++ Ex4/Ex4/Ex4/main.cpp
@@ -16,94 +16,172 @@ using std::endl;
 using std::string;
 using std::istringstream;
 
-int stringtoint(const string s) {
-  istringstream istr(s);
-  int i = 0;
-  istr >> i;
-  return i;
+int stringToInt(const string s) {
+	istringstream istr(s);
+	int i = 0;
+	istr >> i;
+	return i;
 }
 
-double stringtodouble(const string s1,const string s2) {
-  
-  istringstream istr1(s1);
-  istringstream istr2(s2);
-  int i; //integer part
-  int j; //fraction part
-  double a = 0;
-  istr1 >> i;
-  istr2 >> j;
-  a = double(i)+double(j)/1000; //assuming fraction part is alwways 3 digits
-  return a;
+double stringToDouble(const string s1, const string s2) {
+
+	istringstream istr1(s1);
+	istringstream istr2(s2);
+	int i; //integer part
+	int j; //fraction part
+	double a = 0;
+	istr1 >> i;
+	istr2 >> j;
+	a = double(i) + double(j) / 1000; //assuming fraction part is alwways 3 digits
+	return a;
 }
 
 vector<string> tokenize(string line, const char* delim) {
-	unsigned int i, j; 
+	unsigned int i, j;
 	vector<string> tokens;
 	while (!line.empty()) {
 		i = line.find_first_not_of(delim);
-		j = line.find_first_of(delim, i+1);
-		tokens.push_back(line.substr(i, j-i));
-		if (j > line.size()-1)
+		j = line.find_first_of(delim, i + 1);
+		tokens.push_back(line.substr(i, j - i));
+		if (j > line.size() - 1)
 			line = "";
 		else
-			line = line.substr(j+1,line.size()-j-1);
+			line = line.substr(j + 1, line.size() - j - 1);
 	}
 	return tokens;
 }
 
-int main() {
 
-	office kg_office; //kindergarten office
+int main()
+{
+	Office KG_Office; //KinderGarten Office
 	const char* delims = " \t\n";
 	vector<string> tokens;
 	string line;
-	unsigned int linenumber = 1;
+	const char* dotDelim = ".";
+	vector<string> tokens, small_tokens;
+	string line, name, phone;
+	unsigned int lineNumber = 1;
 
+	int size, age, childMax, seniority;
+	double ratio;
 	while (!cin.eof()) {
-	  getline(cin, line);
-	  tokens = tokenize(line, delims);
-	  if (tokens.size() == 0) { //empty line
-	    continue;
-	  }
-	  
-	  if (tokens[0] == "addclass") {
-	    //add your code here ...
-	  }
-	  if (tokens[0] == "removeclass") {
-	    //add your code here ...
-	  }
-	  if (tokens[0] == "addchild") {
-	    //add your code here ...
-	  }
-	  
-	  if (tokens[0] == "addteahcer") {
-	    //add your code here ...
-	  }
-	  
-	  if (tokens[0] == "removechild") {
-	    //add your code here ...
-	  }
-	  
-	  if (tokens[0] == "removeteacher") {
-	    //add your code here ...
-	  }
-	  
-	  if (tokens[0] == "printkindergarten") {
-	    //add your code here ...
-	  }
-	  
-	  if (tokens[0] == "sickchild") {
-	    //add your code here ...
-	  }
-	  
-	  linenumber++;
+		getline(cin, line);
+		tokens = tokenize(line, delims);
+		if (tokens.size() == 0) { //empty line
+			if (tokens.size() == 0) //empty line
+			{
+				continue;
+			}
+			if (tokens[0] == "addClass")
+			{
+				if (empty(tokens[1]) || empty(tokens[2]) || empty(tokens[3]) || empty(tokens[4]) || !empty(tokens[5]))
+				{
+					cerr << "Failed - " << line << endl;
+					continue;
+				}
+				size = stringToInt(tokens[1]);
+				small_tokens = tokenize(tokens[2], dotDelim);
+				if (!empty(small_tokens[3]) || empty(small_tokens[1]) || empty(small_tokens[2]) )
+				{
+					cerr << "Failed - " << line << endl;
+					continue;
+				}
+				ratio = stringToDouble(small_tokens[0], small_tokens[1]);
+				childMax = stringToInt(tokens[3]);
+				age = stringToInt(tokens[4]);
+				KG_Office.addClass(size, ratio, childMax, age);
+			}
+			if (tokens[0] == "removeClass")
+			{
+				if (empty(tokens[1]) || !empty(tokens[2]))
+				{
+					cerr << "Failed - " << line << endl;
+					continue;
+				}
+				age = stringToInt(tokens[1]);
+				if (KG_Office.removeClass(age) == FAILURE) 
+					cerr << "Failed - " << line << endl;
+			}
+			if (tokens[0] == "addChild")
+			{
+				if (empty(tokens[1]) || empty(tokens[2]) || empty(tokens[3]) || !empty(tokens[4]))
+				{
+					cerr << "Failed - " << line << endl;
+					continue;
+				}
+				age = stringToInt(tokens[1]);
+				name = tokens[2];
+				phone = tokens[3];
+				if (KG_Office.addChild(name, age, phone) == FAILURE) 
+					cerr << "Failed - " << line << endl;
+			}
+			if (tokens[0] == "addTeahcer")
+			{
+				if (empty(tokens[1]) || empty(tokens[2]) || empty(tokens[3]) || !empty(tokens[4]))
+				{
+					cerr << "Failed - " << line << endl;
+					continue;
+				}
+				age = stringToInt(tokens[1]);
+				name = tokens[2];
+				seniority = stringToInt(tokens[3]);
+				if (KG_Office.addTeacher(name, age, seniority) == FAILURE)   
+					cerr << "Failed - " << line << endl;
+			}
+			if (tokens[0] == "removeChild")
+			{
+				if (empty(tokens[1]) || !empty(tokens[2]))
+				{
+					cerr << "Failed - " << line << endl;
+					continue;
+				}
+				name = tokens[1];
+				if (KG_Office.removeChild(name) == FAILURE)  
+					cerr << "Failed - " << line << endl;
+			}
+			if (tokens[0] == "removeTeacher")
+			{
+				if (empty(tokens[1]) || !empty(tokens[2]))
+				{
+					cerr << "Failed - " << line << endl;
+					continue;
+				}
+				name = tokens[1];
+				if (KG_Office.removeTeacher(name) == FAILURE)  
+					cerr << "Failed - " << line << endl;
+			}
+			if (tokens[0] == "PrintKindergarten")
+			{
+				if (!empty(tokens[1]))
+				{
+					cerr << "Failed - " << line << endl;
+					continue;
+				}
+				KG_Office.print();
+			}
+			if (tokens[0] == "sickChild")
+			{
+				if (empty(tokens[1]) || !empty(tokens[2]))
+				{
+					cerr << "Failed - " << line << endl;
+					continue;
+				}
+				name = tokens[1];
+				if (KG_Office.reportSick(name) == FAILURE)    
+					cerr << "Failed - " << line << endl;
+			}
+			lineNumber++;
+		}
 	}
-	return 0; 
-};
+	return 0;
+}
 
-#include "Room.H"
 
-int main() {
+
+//#include "Room.H"
+
+//int main() {
 
 	//Room r(15, 60);
 	//r.print();
@@ -121,5 +199,5 @@ int main() {
 	//c.removeTeacher("Michle");
 	//c.print();
 	//return 0;
-}
+//}
 
