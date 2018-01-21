d9047c14d072d8b69a911771b633dab4c372b74a
DeanBarzilay <34457749+DeanBarzilay@users.noreply.github.com>
2018-01-22 01:46:31 +0200

    sharedptr

--- Ex5/Ex5/Ex5/MySharedPtr.h
+++ Ex5/Ex5/Ex5/MySharedPtr.h
@@ -1,9 +1,40 @@
 #ifndef _MY_SHARED_PTR_H_
 #define _MY_SHARED_PTR_H_
 
-
-
-
-
+template <class T>
+class MySharedPtr
+{
+	T* ptr;
+	int* counter;
+public:
+	MySharedPtr(T* p = 0) : ptr(NULL) , counter(NULL) 
+	{
+		if (p.ptr)
+		{
+			this->ptr = p.ptr;
+			this->counter = p.counter;
+			counter++;
+		}
+		else
+		{
+			this->ptr = new T*;
+			this->counter = new int*;
+			this->counter = 1;
+		}
+	}
+	~MySharedPtr() 
+	{
+		if (this->counter == 1)
+		{
+			delete this->ptr;
+			delete this->counter;
+		}
+		else
+			this->counter--;
+	}
+	T& operator*() { return *ptr; }
+	T* operator->() { return ptr; }
+	T* get() { return ptr; }
+};
 
 #endif // _MY_SHARED_PTR_H_
