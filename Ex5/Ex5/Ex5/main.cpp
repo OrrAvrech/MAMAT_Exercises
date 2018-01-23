c4ee2e054b6f2c2f18737452d754a43e51ef1eb5
OrrAvrech <orravrech@gmail.com>
2018-01-24 00:06:27 +0200

    main to check Conversation

--- Ex5/Ex5/Ex5/main.cpp
+++ Ex5/Ex5/Ex5/main.cpp
@@ -1,35 +1,50 @@
 #include <iostream>
 #include <exception>
 #include <string>
-#include "ChatNet.h"
+#include <set>
+#include <vector>
+#include <map>
+#include <chrono>
+#include "Conversation.h"
 using namespace std;
 
-
-
 int main() {
-	string line;
-	ChatNet MamatChat("MamatChat", "Admin", "1234");
-	while (!cin.eof())
-	{
-		try
-		{
-			getline(cin, line);
-			MamatChat.Do(line);
-		}
-		catch (BackSignal&) // Exit
-		{
-			break;
-		}
-		catch (exception& e)
-		{
-			cout << e.what() << endl;
-		}
-		catch (...)
-		{
-			cout << "Unexpected Error" << endl;
-			return 1;
-		}
-	}
+	//string line;
+	//chatnet mamatchat("mamatchat", "admin", "1234");
+	//while (!cin.eof())
+	//{
+	//	try
+	//	{
+	//		getline(cin, line);
+	//		mamatchat.do(line);
+	//	}
+	//	catch (backsignal&) // exit
+	//	{
+	//		break;
+	//	}
+	//	catch (exception& e)
+	//	{
+	//		cout << e.what() << endl;
+	//	}
+	//	catch (...)
+	//	{
+	//		cout << "unexpected error" << endl;
+	//		return 1;
+	//	}
+	//}
+	set<string> part = { "user_23", "bla", "maso" };
+	vector<Message> msgList;
+	msgList.clear();
+	map<string, ConversationStatus> readLst = {
+		{"user_23", UNREAD},
+		{"bla", UNREAD},
+		{"maso", UNREAD}
+	};
+	SysTime lastTime;
 
+	Conversation c(part, msgList, readLst, lastTime);
+	c.VrtDo("Write whatup?", "bla");
+	c.VrtDo("Write ok!", "maso");
+	c.Preview("user_23");
 	return 0;
 };
